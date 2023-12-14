f70645be738d204e42ea04e653612b0dd3c327f0
Ok-michael <okomichaelakpu@gmail.com>
2023-12-14 21:13:49 +0100

    create all the necessary files

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include "monty.h"
 
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
- * main - this is the entry point to the monty code interpreter
- * @argc: Argument count
- * @argv: the second element of the commandline arguments which
- * is the monty file location.
+ * main - the entry point to the monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
  * Return: 0 on success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	ssize_t lineRead = 1;
-	stack_t *stack = NULL;
-	unsigned int cnt = 0;
 	char *content;
 	FILE *file;
 	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
@@ -25,19 +23,21 @@ int main(int argc, char **argv)
 	}
 	file = fopen(argv[1], "r");
 	bus.file = file;
-	if (file == NULL)
+	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (lineRead > 0)
+	while (read_line > 0)
 	{
 		content = NULL;
-		lineRead = getline(&content, &size, file);
+		read_line = getline(&content, &size, file);
 		bus.content = content;
-		cnt++;
-		if (lineRead > 0)
-			execute(content, &stack, cnt, file);
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
 		free(content);
 	}
 	free_stack(stack);
